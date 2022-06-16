c29f7fb76176da6171db6c39379259e624184b36
s3un9s00n <93629439+s3un9s00n@users.noreply.github.com>
2022-06-16 23:59:53 +0900

    feat: Create index.html

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Position = require('./routes/position');
+
+app.listen(3000, function () {
+    console.log('start');
+});
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine','ejs')
+app.use('/BusMap', Position);
+
+app.get('/BusMap', function (req,res) {
+    res.sendFile(__dirname + '/public/main.html');
+})
---
+++ routes/position.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+router.get('/', function (req, res) {
+
+});
+
+module.exports = router();
